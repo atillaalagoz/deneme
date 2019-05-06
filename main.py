9663122a23850b2dfbea6fe471ca9c8a2defc26a
Atilla Alagöz <atillaalagoz@gmail.com>
2019-05-06 12:31:13 +0300

    dd

--- main.py
+++ main.py
@@ -1,22 +1,4 @@
-def topla(x, y):
+def carp(x, y):
     return x + y
-
-def f2():
-    print("12")
-    print("122")
-
-
-def topla(x, y):
+def carp(x, y):
     return x + y
-
-def f2():
-    print("12")
-    print("122")
-
-
-def topla(x, y):
-    return x + y
-
-def f2():
-    print("12")
-    print("12 2")
