2b546ff3386d9598e2f1a68c9d67129b24d82950
Atilla Alagöz <atillaalagoz@gmail.com>
2019-05-06 12:14:46 +0300

    bb

--- main.py
+++ main.py
@@ -4,4 +4,19 @@ def topla(x, y):
 def f2():
     print("12")
     print("122")
-    
+
+
+def topla(x, y):
+    return x + y
+
+def f2():
+    print("12")
+    print("122")
+
+
+def topla(x, y):
+    return x + y
+
+def f2():
+    print("12")
+    print("122")
