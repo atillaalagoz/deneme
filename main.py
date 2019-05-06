96eff4c0ffe28b457ec26803fdfc44775d03626e
Atilla Alagöz <atillaalagoz@gmail.com>
2019-05-06 12:34:19 +0300

    mdmd

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
-def carp(x, y):
-    return x + y
-def carp(x, y):
-    return x + y
+import scrapy
+from scrapy.crawler import CrawlerProcess
+
+
+class QuotesScraper(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'http://quotes.toscrape.com/tag/humor/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'aıthor': quote.xpath('span/small/text()').get()
+            }
+
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+
+
+crawler_process = CrawlerProcess({
+        'USER_AGENT':
+'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'
+})
+
+crawler_process.crawl(QuotesScraper)
+crawler_process.start()
